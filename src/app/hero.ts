bdd2b0115d54301e54c67fb1f2feb3e820734914
jhonsef <jrp.ctt@gmail.com>
2018-08-19 05:12:15 -0300

    [add](HeroForm): adiciona o componente para o HeroForm

--- src/app/hero.ts
+++ src/app/hero.ts
@@ -1,2 +1,10 @@
+//It's an anemic model with few requirements and no behavior. 
+//Perfect for the demo.
 export class Hero {
+    constructor(
+        public id: number,
+        public name: string,
+        public power: string,
+        public alterEgo?: string
+    ){}
 }
